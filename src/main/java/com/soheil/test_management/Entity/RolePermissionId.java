0059ec085f74471053dd0d92634a611e2149a6e8
soheil <soheilkooshan710@gmail.com>
2025-08-25 02:50:01 +0330

    enity created

---
+++ src/main/java/com/soheil/test_management/Entity/RolePermissionId.java
@@ -0,0 +1,36 @@
+package com.soheil.test_management.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Embeddable
+public class RolePermissionId implements java.io.Serializable {
+    private static final long serialVersionUID = 5545078516568087829L;
+    @Column(name = "role_id", nullable = false)
+    private Integer roleId;
+
+    @Column(name = "permission_id", nullable = false)
+    private Integer permissionId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        RolePermissionId entity = (RolePermissionId) o;
+        return Objects.equals(this.permissionId, entity.permissionId) &&
+                Objects.equals(this.roleId, entity.roleId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(permissionId, roleId);
+    }
+
+}
---
+++ src/main/java/com/soheil/test_management/Entity/TestParameter.java
@@ -0,0 +1,33 @@
+package com.soheil.test_management.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "test_parameters")
+public class TestParameter {
+    @Id
+    @Column(name = "test_parameter_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "template_id", nullable = false)
+    private TestTemplate template;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "unit", nullable = false, length = 50)
+    private String unit;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "expected_range_id")
+    private ExpectedRange expectedRange;
+
+    @Column(name = "required", nullable = false)
+    private Boolean required = false;
+
+}
