a930c011d786b3514c7af8ba479ca52dcef95655
unknown <soheilkooshan710@gmail.com>
2025-09-06 18:43:00 +0330

    Exception handling in one commit

---
+++ server/src/main/java/com/soheil/test_management/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,2 @@
+package com.soheil.test_management.exceptions;public class ResourceNotFoundException {
+}
---
+++ server/src/main/java/com/soheil/test_management/payloads/ErrorResponse.java
@@ -0,0 +1,2 @@
+package com.soheil.test_management.payloads;public class ErrorResponse {
+}
